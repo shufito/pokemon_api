d49f0819954cb235f695e2b3b03f03f589db76a2
shufito <jshufito@gmail.com>
2023-04-08 13:04:34 -0300

    v1.2

--- src/componentes/pokemonDetails.jsx
+++ src/componentes/pokemonDetails.jsx
@@ -28,10 +28,15 @@ if(dados.length === 0 ){
                 <div className="col-lg-6 text-center text-lg-start fundo-img mt-0">
                     <img src={dados.data.sprites.other["official-artwork"].front_default} alt="ok" className="w-100 imagem-fluid"/>
                 </div>
-                <div className="col-md-6 mx-auto col-lg-6 mt-0">
+                <div className="col-md-12 mx-auto col-lg-6 mt-0">
                     <div className='row row-cols-2'>
                         <div className='col-12'>
-                            <h2 className='display-6 fw-bold '>{dados.data.name}</h2>
+                            <h2 className='display-6 fw-bold '>
+                            {(dados.data.name).charAt(0).toUpperCase() + (dados.data.name).slice(1)}
+                            <span className='fs-4 text-muted fw-normal ms-2'>
+                                #{(dados.data.id).toString().padStart(3, '0')}
+                            </span>
+                            </h2>
                         </div>
                         <div>
                             <h3 className="fw-bold mb-0 fs-4">Altura</h3>
@@ -47,15 +52,15 @@ if(dados.length === 0 ){
                         </div>
                         <div>
                             <h3 className="fw-bold mb-0 fs-4">Tipos</h3>
-                            <p className=''>{dados.data.types.map(typeInfo => (<span key={typeInfo.type.name} className={`badge fs-6 fw-normal me-2 ${typeInfo.type.name}`} >{typeInfo.type.name}</span>))}</p>  
+                            <p className='my-1'>{dados.data.types.map(typeInfo => (<span key={typeInfo.type.name} className={`badge fs-6 fw-normal my-1 me-2 ${typeInfo.type.name}`} >{typeInfo.type.name}</span>))}</p>  
                         </div> 
                     </div>
                     {dados.data.stats.map(statsInfo => (
                         <div key={statsInfo.stat.name} className='my-2'>
                             <span>{statsInfo.stat.name}</span>
                             <div className="progress"  role="progressbar" aria-label={"label"} aria-valuenow={0}aria-valuemin={0} aria-valuemax={100} >
-                                <div className="progress-bar" style={{ width: `${((statsInfo.base_stat - 0) / (200 - 0)) * 100}%` }}>
-                                    {"label" && <span className="progress-label" style={{left: `${((statsInfo.base_stat - 7) / (200 - 0)) * 100}%`}}>{`${statsInfo.base_stat}`}</span>}
+                                <div className={`badge fs-6 fw-normal me-2 ${dados.data.types[0].type.name}`} style={{ width: `${((statsInfo.base_stat - 0) / (200 - 0)) * 100}%` }}>
+                                    {"label" && <span className="progress-label fw-bold" style={{left: `${((statsInfo.base_stat - 5) / (200 - 0)) * 100}%`}}>{`${statsInfo.base_stat}`}</span>}
                                 </div>
                             </div>
                         </div>
